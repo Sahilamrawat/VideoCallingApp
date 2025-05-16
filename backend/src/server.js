7073d51f95f74adf4d6018c41290d0ca2d48e743
Sahilamrawat <sahil23462@iiitd.ac.in>
2025-05-16 22:33:08 +0530

    Deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,13 @@ import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import path from "path";
 import cors from 'cors';
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 3000;
+
+const __dirname=path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -19,6 +22,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat",chatRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
